a2c27f6bb934b68e4232ccb607fae507b08c51e9
kira-1011 <kirubel.s.tadesse@gmail.com>
2024-04-14 14:32:18 +0300

    create Dog and Breed models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Breed(models.Model):
+
+    name = models.CharField(max_length=100)
+    size = models.CharField(max_length=1,  choices=[(
+        'Tiny',), ('Small',), ('Medium',), ('Large',)])
+    friendliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    trainability = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    sheddingamount = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    exerciseneeds = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.name
+
+
+class Dog(models.Model):
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=1,  choices=[
+                              ('M', 'male'), ('F', 'female')])
+    color = models.CharField(max_length=100)
+    favoritefood = models.CharField(max_length=100)
+    favoritetoy = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
